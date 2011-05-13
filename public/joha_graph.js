81fb05edb6e2f696282f57013554a4db3d818efb
Dave M <dmarti21@gmail.com>
2011-05-13 21:02:25 +0000

    WIP Progress on dynamic creation, but more to go

--- public/joha_graph.js
+++ public/joha_graph.js
@@ -82,15 +82,106 @@ function toggle(divId) {
 }
 
 //Dynamic element creation based on data structure
+//-- common look and feel 
+var file_elements_format = function(filenames, divIdLabel, el){
+  if (filenames.length > 0 ){
+    //create the DOM elements
+    for (i in filenames){
+      jQuery("<div />", {
+        "id":divIdLabel + i,
+        text: filenames[i],
+        click: function(){get_current_node_attachment(filenames[i])}
+      }).appendTo(el);
+    }
+    
+  //alert("Attached filename: " + filename + " in special html");
+  } else {
+    alert('zero length filenames passed');
+  }
+}
+
+var link_elements_format = function(links, divIdLabel, el){
+  var linkHtml = ""
+  for (url in links){
+    var indivLabels = links[url]
+    if (indivLabels instanceof Array){
+      /* ok */
+      } else {
+      indivLabels = [indivLabels] //make array
+    }
+    for (i in indivLabels){ 
+     linkHtml += "<a href=\"" + url + "\">" + indivLabels[i] + "</a></br>"
+     //make bigger html
+    }
+  }
+  
+  //Build Dom Element
+  jQuery("<div />", {
+    "id": divIdLabel,
+    html: linkHtml
+  }).appendTo(el);
+}
+
+//needs testing
+var static_elements_format = function(staticKey, staticVal, divIdLabel, el){
+  jQuery("<div />", {
+    "id":divIdLabel,
+    html: "<h1> " + staticKey + " " + staticVal + "  </h1>",
+   }).appendTo(el);
+}
+
+var replace_elements_format = function(replaceKey, replaceVal, divIdLabel, el){
+  jQuery("<div />", {
+    "id": divIdLabel,
+    html: "<span class=\"replace_key\">" + replaceKey + "</span>: <span id=\"replace_val\">" + replaceVal + "</span>",
+   }).appendTo(el);
+}
+
+var list_elements_format = function(listKey, listData, divIdLabel, el){
+  var listHtml = "<ul>List of " + listKey
+  for( i in listData){
+    listHtml += "\n <li>" + listData[i] + "</li>\n"
+  }
+  listHtml += "</ul>"
+  jQuery("<div />", {
+    "id": divIdLabel,
+    html: listHtml,
+   }).appendTo(el);
+}
+
+var key_list_elements_format = function(keyList, divIdLabel, el){
+  alert('key list format');
+  /*for( mainKey in keyList ){
+      var divIdKeyLabel = divIdLabel + "_" + mainKey
+      var listData = keyList[mainKey]
+      var listHtml = "<ul>Key List of " + mainKey
+      for(  listKey in listData){
+        listHtml = "<li><ul>List of " + listKey
+        for (i in listData[listKey] ){
+          listHtml += "\n <li>" + listData[listKey][i] + "</li>\n"
+        }
+        listHtml += "</ul></li>"
+      }
+      listHtml += "</ul"
+      jQuery("<div />", {
+        "id": divIdKeyLabel,
+        html: listHtml,
+      }).appendTo(el);
+  }*/
+}
+
+//-- creation
 function dynamic_edit_form(nodeData){
 
   //Define the functions that will display based on node data keys
   
   //-- Some data keys are special and get their own specific display function
   var edit_id_elements = function(id){
-    //Note that the DOM eleemnt for id already exists, we're just inserting data into it.
-    jQuery('#current_node_id').text(id);
-  }
+    var current_node_id = jQuery('#current_node_id').text();
+      if (current_node_id == id) { /* ok */} else {
+        alert("Current Node Id: " + current_node_id + " BUT Node Data has id: " + id + ".");
+      }
+    }
 
   var edit_label_elements = function(label){
     //Note that the DOM eleemnt for id already exists, we're just inserting data into it.
@@ -101,37 +192,39 @@ function dynamic_edit_form(nodeData){
   var edit_file_elements = function(filenames){
     //simpler code, but best behavior to make sure selecting node multiple times creates multiple data?
     jQuery('#edit_file_elements').remove();
-    if (filenames.length > 0 ){
-      //create the DOM elements
-      for (i in filenames){
-        jQuery("<div />", {
-          "id":"edit_file_elements_" + i,
-          text: filenames[i],
-          click: function(){alert(filenames[i])}
-        }).appendTo( jQuery('#edit-node-data') );
-      }
-      
-    //alert("Attached filename: " + filename + " in special html");
-    } else {
-      alert('zero length filenames passed');
-    }
+    file_elements_format(filenames, "edit_file_elements_", jQuery('#dn_file_data') );
   }
 
+    var edit_link_elements = function(links){
+    //simpler code, but best behavior to make sure selecting node multiple times creates multiple data?
+    jQuery('#edit_link_elements').remove();
+    link_elements_format(links, "edit_link_elements", jQuery('#dn_link_data') );
+  }
+  
   //-- Other data keys can fall into some generic categories for display functions
+  //-- Static Needs TESTING
   var edit_static_elements = function(staticVar){
-    alert("Static Var: " + staticVar + " handled statically");
+    alert("Static Var: " + staticVar.key + " handled statically");
   }
 
   var edit_replace_elements = function(replaceVar){
-    alert("Replace Var: " + replaceVar + " will be replacable");
+    replaceKey = get_keys(replaceVar)[0];
+    replaceData = replaceVar[replaceKey];
+    var divIdLabel = "edit_" + replaceKey
+    replace_elements_format(replaceKey, replaceData, divIdLabel, jQuery('#dn_node_data'));
   }
 
   var edit_list_elements = function(listVar){
-    alert("List Var: " + listVar + " will be listable");
+    //alert(get_keys(listVar));
+    listKey = get_keys(listVar)[0]
+    listItems = listVar[listKey]
+    var divIdLabel = "list_" + listKey
+    list_elements_format(listKey, listItems, divIdLabel, jQuery('#dn_node_data'));
   }
 
   var edit_keylist_elements = function(keyListVar){
-    alert("Key List Var: " + keyListVar + " will be keyable and listable");
+    var divIdLabel = "edit_keylist_"
+    key_list_elements_format(keyListVar, divIdLabel, jQuery('#dn_node_data'));
   }
   
   //-- We may have node data that has no defined treatment
@@ -146,6 +239,7 @@ function dynamic_edit_form(nodeData){
   //these keys will get special treatment for displaying their structue
   var SPECIAL_TREATMENT = {"id": edit_id_elements,
                            "label": edit_label_elements,
+                           "links": edit_link_elements,
                            "attached_files": edit_file_elements}
   
   //-- operations are defined as part of data defn, this binds data structure (data defn)
@@ -156,22 +250,44 @@ function dynamic_edit_form(nodeData){
                    "key_list_ops": edit_keylist_elements}
                    
   //main algorithm
+  //-- remove any existing data in the dynamic divs
+  jQuery('#dn_node_data').empty();
+  jQuery('#dn_link_data').empty();
+  jQuery('#dn_file_data').empty();
   //-- create a clone of the node data because we are going to be changing it
   //-- but only for display reasons
   var nodeCopy = jQuery.extend({}, nodeData);
   
   var nodeKeys = get_keys(nodeCopy);
   
-  if (array_contains_all(nodeKeys, REQUIRED_DATA)) { /* ok */ } else {
-  alert("Not all Required Data Elements are present in Node Data") };
+  //if (array_contains_all(nodeKeys, REQUIRED_DATA)) { /* ok */ } else {
+  //alert("Not all Required Data Elements are present in Node ID: " + nodeCopy.id + "Keys:" + nodeKeys) };
   
   for (key in SPECIAL_TREATMENT) {
     // - remove - alert(key);
-    SPECIAL_TREATMENT[key](nodeCopy[key]);
-    delete nodeCopy[key];  // we've handled it so let's not worry about it anymore
+    if (nodeCopy[key]){
+      SPECIAL_TREATMENT[key](nodeCopy[key]);
+      delete nodeCopy[key];  // we've handled it so let's not worry about it anymore
+    }
   }
   
+  for (key in nodeCopy) { 
+  //if our data defintion exists for that key, use the appropriate function for displaying it
+  if (JOHA_DATA_DEF[key]) {
+    var edit_ops_var = {}
+    edit_ops_var[key] = nodeCopy[key]
+    EDIT_OPS_MAP[(JOHA_DATA_DEF[key])](edit_ops_var);
+    delete nodeCopy[key];
+  }
+}
+  
   alert(get_keys(nodeCopy));
+}
+
+//functions dealing with attached files
+function get_current_node_attachment(filename){
+  var currentNodeId = jQuery('#current_node_id').text();
+  alert("Current Node: " + currentNodeId + " Filename: " + filename + ".");
 }
     
 //Graphing helpers and interactions
@@ -202,7 +318,10 @@ function routeClickedNodeDataToElements(nodeStale) {
   //parentCatEditBox.value = node.data.parents;
   // - remove - nodeIdBox.innerHTML = node.id;
   //assign current working node id
-  // - remove?- jQuery('#current_node_id').text(node.id);
+  
+  //Need this here for parents that are not nodes
+  jQuery('#current_node_id').text(node.id);
+  
   // - remove - alert($('#node_id_edit').value);
   // - remove - alert(jQuery('#current_node_id').html());
   // - remove - nodeIdBoxLabel.innerHTML = node.name;
@@ -221,6 +340,7 @@ function add_descendant_data(el, node_data_type){
   //var attach_name = el.previousSibling.innerHTML;
   //var node_id = $('node_id_edit_label').innerHTML;
   var node_id = jQuery('#current_node_id').text();
+  //alert(node_id);
   //using jQuery
   el.load('/desc_data', {'node_id': node_id, 'node_data_type':node_data_type });
 //  new Ajax.Updater(el, '/desc_data', {'node_id': node_id, 'node_data_type':node_data_type });
