d7f558e94258361823bb6b4240d496b2916b4b86
Dave M <dmarti21@gmail.com>
2011-05-13 16:33:06 +0000

    WIP Dynamically creating elements

--- public/joha_graph.js
+++ public/joha_graph.js
@@ -2,12 +2,14 @@
 
 // Something about initialization or getting ready, something something
 $(document).ready(function() {
+  //Constants
   JOHA_DATA_DEF = syncJax('/data_definition');
+
   initializePage();
   
 });
 
-//Initialization Functions
+//Initialization Functions  TODO: Change to function form (lowercase underscore)
 function syncJax(srcUrl) {
   retVal = "";
   
@@ -17,7 +19,35 @@ function syncJax(srcUrl) {
   });
   
   return retVal
+}
+
+//Generic Helper functions
+function get_keys(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys;
 }  
+
+function array_contains(a, obj) {
+  var i = a.length;
+  while (i--) {
+    if (a[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function array_contains_all(a, subset){
+  var retVal = true;
+  for (var i in subset){
+    var obj = subset[i]
+    if ( array_contains(a, obj) ){} else { retVal = false;};
+  }
+  return retVal
+}
 
 function initializePage(){
   blankGraph = rgraphInit(); //insert canvas into here if you can figure it out
@@ -25,7 +55,7 @@ function initializePage(){
   var nodeSource = '/index_nodes';
   //the below assigns the graph to myGraph (via Ajax)
   insertNodesIntoGraph(blankGraph, nodeSource);
-  jQuery('node_id_input_edit').value = " ";
+  // - remove? - jQuery('node_id_input_edit').value = " ";
   //setAuthToken('authtok_attach_form');  
 }
     
@@ -35,9 +65,9 @@ function show_create_node_form(){
   jQuery('#create-node-form').show();
 }
 
-function show_edit_node_form(nodeId){
-  jQuery('#node_id_input_edit').text(nodeId);
+function show_edit_node_form(node){
   jQuery('#create-node-form').hide();
+  dynamic_edit_form(node.data);
   jQuery('#edit-node-form').show();
 }
 
@@ -49,6 +79,99 @@ function toggle(divId) {
   	}
   else { ele.style.display = "block";
   }
+}
+
+//Dynamic element creation based on data structure
+function dynamic_edit_form(nodeData){
+
+  //Define the functions that will display based on node data keys
+  
+  //-- Some data keys are special and get their own specific display function
+  var edit_id_elements = function(id){
+    //Note that the DOM eleemnt for id already exists, we're just inserting data into it.
+    jQuery('#current_node_id').text(id);
+  }
+
+  var edit_label_elements = function(label){
+    //Note that the DOM eleemnt for id already exists, we're just inserting data into it.
+    jQuery('#node_id_edit_label').text(node.name);
+  }
+  
+  //TODO: Create file object with filename and file data (if needed)
+  var edit_file_elements = function(filenames){
+    //simpler code, but best behavior to make sure selecting node multiple times creates multiple data?
+    jQuery('#edit_file_elements').remove();
+    if (filenames.length > 0 ){
+      //create the DOM elements
+      for (i in filenames){
+        jQuery("<div />", {
+          "id":"edit_file_elements_" + i,
+          text: filenames[i],
+          click: function(){alert(filenames[i])}
+        }).appendTo( jQuery('#edit-node-data') );
+      }
+      
+    //alert("Attached filename: " + filename + " in special html");
+    } else {
+      alert('zero length filenames passed');
+    }
+  }
+
+  //-- Other data keys can fall into some generic categories for display functions
+  var edit_static_elements = function(staticVar){
+    alert("Static Var: " + staticVar + " handled statically");
+  }
+
+  var edit_replace_elements = function(replaceVar){
+    alert("Replace Var: " + replaceVar + " will be replacable");
+  }
+
+  var edit_list_elements = function(listVar){
+    alert("List Var: " + listVar + " will be listable");
+  }
+
+  var edit_keylist_elements = function(keyListVar){
+    alert("Key List Var: " + keyListVar + " will be keyable and listable");
+  }
+  
+  //-- We may have node data that has no defined treatment
+  //-- in this approach we still allow editing of that data, but because
+  //-- we don't know the structure, we defer to the user to make sure it's
+  //-- the data form is correct (i.e. braces for maps, brackets for arrays, quotes for embedded strings, etc)
+  var edit_no_data_def = edit_replace_elements;
+  
+  //these keys are required to be present in the node data
+  var REQUIRED_DATA = ["id", "label"]  //may be able to refactor label out in the future
+  
+  //these keys will get special treatment for displaying their structue
+  var SPECIAL_TREATMENT = {"id": edit_id_elements,
+                           "label": edit_label_elements,
+                           "attached_files": edit_file_elements}
+  
+  //-- operations are defined as part of data defn, this binds data structure (data defn)
+  //-- to specific js operations for displaying that structure
+  var EDIT_OPS_MAP = {"static_ops": edit_static_elements,
+                   "replace_ops": edit_replace_elements,
+                   "list_ops": edit_list_elements,
+                   "key_list_ops": edit_keylist_elements}
+                   
+  //main algorithm
+  //-- create a clone of the node data because we are going to be changing it
+  //-- but only for display reasons
+  var nodeCopy = jQuery.extend({}, nodeData);
+  
+  var nodeKeys = get_keys(nodeCopy);
+  
+  if (array_contains_all(nodeKeys, REQUIRED_DATA)) { /* ok */ } else {
+  alert("Not all Required Data Elements are present in Node Data") };
+  
+  for (key in SPECIAL_TREATMENT) {
+    // - remove - alert(key);
+    SPECIAL_TREATMENT[key](nodeCopy[key]);
+    delete nodeCopy[key];  // we've handled it so let's not worry about it anymore
+  }
+  
+  alert(get_keys(nodeCopy));
 }
     
 //Graphing helpers and interactions
@@ -79,13 +202,13 @@ function routeClickedNodeDataToElements(nodeStale) {
   //parentCatEditBox.value = node.data.parents;
   // - remove - nodeIdBox.innerHTML = node.id;
   //assign current working node id
-  jQuery('#current_node_id').text(node.id);
+  // - remove?- jQuery('#current_node_id').text(node.id);
   // - remove - alert($('#node_id_edit').value);
   // - remove - alert(jQuery('#current_node_id').html());
   // - remove - nodeIdBoxLabel.innerHTML = node.name;
-  jQuery('#node_id_edit_label').text(node.name);
+  // - remove - jQuery('#node_id_edit_label').text(node.name);
   //functions to distribute data to 
-  show_edit_node_form(node.id);
+  show_edit_node_form(node);
   //TODO: Make this dynamic based on dataset
   add_descendant_data(jQuery('#desc-nodes'), 'label');
   add_descendant_data(jQuery('#desc-files'), 'attached_files');
